427991495a3e774f1ad65e259013a9e0a329a031
henriette <henrietteharmse.dariel@gmail.com>
2018-03-06 11:39:14 +0200

    Eclipse template

---
+++ template/eclipse/source/tutorial/src/main/java/org/jena/tutorial/BaseClassesMain.java
@@ -0,0 +1,40 @@
+package org.jena.tutorial;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.util.iterator.ExtendedIterator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
+public class BaseClassesMain {
+	private static Logger logger = LoggerFactory.getLogger(BaseClassesMain.class);
+	// Why This Failure marker
+	private static final Marker WTF_MARKER = MarkerFactory.getMarker("WTF");
+	
+	private static final String NS_OWL  = "http://www.semanticweb.org/henriette/ontologies/2018/2/untitled-ontology-742#";
+	
+	public static void main(String[] args) {
+		try {		
+			Path path = Paths.get(".").toAbsolutePath().normalize();
+			String ontologyFile = "file:" + path.toFile().getAbsolutePath() + "/src/main/resources/BaseClasses.owl";
+			
+			OntModel defaultModel = ModelFactory.createOntologyModel();
+			defaultModel.read(ontologyFile);
+			logger.debug("Model = " + defaultModel);
+			
+			for (ExtendedIterator<OntClass> i =  defaultModel.listHierarchyRootClasses(); i.hasNext();) {
+				OntClass ontClass = i.next();
+				logger.debug("Base class = " + ontClass);
+			}
+			
+		} catch (Throwable t) {
+			logger.error(WTF_MARKER, t.getMessage(), t);
+		}		
+	}
+}
