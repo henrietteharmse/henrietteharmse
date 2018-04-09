f8edfc869fa6cb732ef4a3cbfd53af67d6b9ddfb
henriette <henrietteharmse.dariel@gmail.com>
2018-04-09 15:56:48 +0200

    TDB2 Create, Write and Read Dataset

--- blog/tutorial/jena/source/tdb/src/main/java/org/jena/tdb/tutorial/TDBCreateWriteRead.java
+++ blog/tutorial/jena/source/tdb/src/main/java/org/jena/tdb/tutorial/TDBCreateWriteRead.java
@@ -20,20 +20,27 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 
-public class TDBRead {
-  private static Logger logger = LoggerFactory.getLogger(TDBRead.class);
+public class TDBCreateWriteRead {
+  private static Logger logger = LoggerFactory.getLogger(TDBCreateWriteRead.class);
   // Why This Failure marker
   private static final Marker WTF_MARKER = MarkerFactory.getMarker("WTF");
-     
+    
   public static void main(String[] args) {
-    try {   
+    try {
+      // Create dataset
       Path path = Paths.get(".").toAbsolutePath().normalize();      
       String dbDir = path.toFile().getAbsolutePath() + "/db/"; 
-      
-      Location location = Location.create(dbDir);
-      
+      Location location = Location.create(dbDir);      
       Dataset dataset = TDB2Factory.connectDataset(location);
-  
+      
+      // Create transaction for writing
+      dataset.begin(ReadWrite.WRITE);
+      UpdateRequest updateRequest = UpdateFactory.create("INSERT DATA {<http://dbpedia.org/resource/Grace_Hopper> <http://xmlns.com/foaf/0.1/name> \"Grace Hopper\" .}");
+      UpdateProcessor updateProcessor = UpdateExecutionFactory.create(updateRequest, dataset);
+      updateProcessor.execute();
+      dataset.commit();
+      
+      // create transaction for reading
       dataset.begin(ReadWrite.READ);
       QueryExecution qe = QueryExecutionFactory.create("SELECT ?s ?p ?o WHERE {?s ?p ?o .}", dataset);
       for (ResultSet results = qe.execSelect(); results.hasNext();) {
@@ -41,7 +48,8 @@ public class TDBRead {
         String strValue = qs.get("?o").toString();
         logger.trace("value = " + strValue);
       }
-
+      
+      // Releasing dataset resources
       dataset.close();
     } catch (Throwable t) {
       logger.error(WTF_MARKER, t.getMessage(), t);
